77da1278e1189756f244a7ddcad5f115ba94e7cf
zs538 <zstulhofer81@gmail.com>
2025-11-20 21:31:10 +0100

    Major touch. Up

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 document.getElementById('blog').addEventListener('click', function(event) {
     event.preventDefault();
-    if (Math.random() < 0.5) {
+    if (Math.random() < 0.75) {
         window.location.href = '/ukolydoskoly';
     } else {
         window.location.href = 'https://www.term.io/';
